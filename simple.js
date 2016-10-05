40d0bdfb01ea3e0d80a7a7b77bc2824f32f11640
zhu8fei <zhu8fei@gmail.com>
2016-10-05 14:04:49 +0800

    暂时没有实现请求头

--- simple.js
+++ simple.js
@@ -1,21 +1,31 @@
 "use strict";
 
 var Proxy = require('./lib/hessian-proxy').Proxy;
+var Long = require('long');
+var Head = require('./lib/hessian-proxy').Head;
+var banana = {
+    "__type__": "com.zhu8fei.bean.Banana",
+    "name": "bbb"
+}
+var listBean = {
+    "__type__": "com.zhu8fei.bean.ListBean",
+    "banana": banana,
+    "str": "sss"
+}
 
-var argObj = {
-    "companyId":2,
-    "data":"test"
-
+var inputDto = {
+    "__type__": "com.odianyun.soa.InputDTO",
+    "companyId": Long.fromInt(2),
+    "data": listBean
 };
 
 
 var proxy = new Proxy('http://127.0.0.1:8080/service/service/test', '', '', proxy);
-argObj.__type__ = 'com.odianyun.soa.InputDTO';
-
-proxy.invoke('simple', [argObj], function (err, reply) {
+proxy.invoke({"method": "getListBean", "head": new Head(2)}, [inputDto], function (err, reply) {
     if (err) {
         console.info('test2: ' + err);
     }
-    console.info(reply);
     console.info('test2: ' + JSON.stringify(reply));
 })
+
+
