b1dc5efea43ba522311379a72458d9937cfc4678
zhu8fei <zhu8fei@gmail.com>
2016-11-07 19:02:23 +0800

    bug 修复.

--- test.js
+++ test.js
@@ -4,29 +4,29 @@ var Proxy = require('./lib/hessian-proxy').Proxy;
 
 // var proxy = new Proxy('http://localhost:8080/ouser-service/ouserService/userLoginService', '', '', proxy);
 /*var str = "30f74983fd7340bebbdfea2a227fb0e9";
-// str.__type__='java.lang.String';
-proxy.invoke('getUserByLoginCookie', [str], function (err, reply) {
-    console.log("err:"+err);
-    console.log('test: ' + JSON.stringify(reply));
-});*/
+ // str.__type__='java.lang.String';
+ proxy.invoke('getUserByLoginCookie', [str], function (err, reply) {
+ console.log("err:"+err);
+ console.log('test: ' + JSON.stringify(reply));
+ });*/
 // 尝试版本1
 // proxy.version=1;
 /*
-var argObj = {
-    "companyId":7,
-    "data":"30f74983fd7340bebbdfea2a227fb0e9"
-};
+ var argObj = {
+ "companyId":7,
+ "data":"30f74983fd7340bebbdfea2a227fb0e9"
+ };
 
-argObj.__type__ = 'com.odianyun.soa.InputDTO';*/
+ argObj.__type__ = 'com.odianyun.soa.InputDTO';*/
 /*var argObj ="30f74983fd7340bebbdfea2a227fb0e9";
 
-proxy.invoke('getUserByLoginCookie', [argObj], function (err, reply) {
-    if (err) {
-        console.info('test2: ' + err);
-    }
-    console.info(reply);
-    console.info('test2: ' + JSON.stringify(reply));
-})*/
+ proxy.invoke('getUserByLoginCookie', [argObj], function (err, reply) {
+ if (err) {
+ console.info('test2: ' + err);
+ }
+ console.info(reply);
+ console.info('test2: ' + JSON.stringify(reply));
+ })*/
 
 
 
@@ -56,32 +56,35 @@ proxy.invoke('getUserByLoginCookie', [argObj], function (err, reply) {
 
 
 var argObj = {
-    "companyId":2,
-    "traceTicket":"fromToNodeClient",
-    "data":{
-        "str":"中文"
+    "companyId": 2,
+    "traceTicket": "fromToNodeClient",
+    "data": {
+        "name": "中文"
     }
 };
-var args =  [argObj];
-args.head = {"companyId_":"10"};
-
-
-var proxy = new Proxy('http://127.0.0.1:8080/service/service/test', '', '', proxy);
+var args = [argObj];
+args.head = {"companyId_": "10"};
 argObj.__type__ = 'com.odianyun.soa.InputDTO';
-argObj.data.__type__='com.zhu8fei.bean.ListBean';
+argObj.data.__type__ = 'com.zhu8fei.test.dto.Banana';
+// argObj.data.__type__ = 'com.zhu8fei.bean.ListBean';
 
-proxy.invoke('getListBean', args , function (err, reply) {
+
+// var proxy = new Proxy('http://127.0.0.1:8080/service/service/test', '', '', proxy);
+// proxy.invoke('getListBean', args , function (err, reply) {
+
+var proxy = new Proxy('http://127.0.0.1:8080/test', '', '', proxy);
+proxy.invoke('getMore', args, function (err, reply) {
     if (err) {
         console.info('test2: ' + err);
     }
     console.info('result: \n\n    ' + JSON.stringify(reply));
 });
 
-/*var proxy = new Proxy('http://127.0.0.1:8080/test', '', '', proxy);
+/*
 
-proxy.invoke('getMore', [], function (err, reply) {
-    if (err) {
-        console.info('test21: ' + err);
-    }
-    console.info('test2: ' + JSON.stringify(reply));
-})*/
+ proxy.invoke('getMore', [], function (err, reply) {
+ if (err) {
+ console.info('test21: ' + err);
+ }
+ console.info('test2: ' + JSON.stringify(reply));
+ })*/
